8bab74bba1d4fcf6f7c63f6cecb7d1184bc34374
Kalle Kankaanpää <kallekan11@gmail.com>
2021-02-17 14:21:14 +0200

    Improved error handling

--- crates/macros/src/lib.rs
+++ crates/macros/src/lib.rs
@@ -2,23 +2,26 @@ extern crate proc_macro;
 
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{self, Data, DeriveInput, Fields};
+use syn::{self, parse_macro_input, Data, DeriveInput, Error, Fields, Result};
 
 #[proc_macro_derive(BlockData)]
 pub fn block_data_derive(input: TokenStream) -> TokenStream {
-    let ast = syn::parse(input).expect("Parsing struct for BlockData macro failed");
+    let ast = parse_macro_input!(input as DeriveInput);
 
-    impl_block_data(&ast)
+    impl_block_data(&ast).unwrap_or_else(|err| err.to_compile_error().into())
 }
 
-fn impl_block_data(ast: &DeriveInput) -> TokenStream {
+fn impl_block_data(ast: &DeriveInput) -> Result<TokenStream> {
     let name = &ast.ident;
     let field;
 
     if let Data::Struct(data) = &ast.data {
         if let Fields::Named(named_fields) = &data.fields {
             if named_fields.named.len() != 1 {
-                panic!("Can't derive BlockData for struct with more than 1 named field")
+                return Err(Error::new(
+                    name.span(),
+                    "Can't derive BlockData for struct with more than 1 named field",
+                ));
             };
             field = named_fields
                 .named
@@ -28,10 +31,16 @@ fn impl_block_data(ast: &DeriveInput) -> TokenStream {
                 .ident
                 .unwrap();
         } else {
-            panic!("Can't derive BlockData for a struct with no named fields")
+            return Err(Error::new(
+                name.span(),
+                "Can't derive BlockData for a struct with no named fields",
+            ));
         }
     } else {
-        panic!("Can't derive BlockData for a non struct")
+        return Err(Error::new(
+            name.span(),
+            "Can't derive BlockData for a non struct",
+        ));
     }
 
     let gen = quote! {
@@ -49,5 +58,5 @@ fn impl_block_data(ast: &DeriveInput) -> TokenStream {
         }
     };
 
-    gen.into()
+    Ok(gen.into())
 }
