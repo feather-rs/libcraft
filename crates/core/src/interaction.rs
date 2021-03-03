0d51f4c9968515d02f194feaaa5ba8af32fa0dfb
Amber Kowalski <amberkowalski03@gmail.com>
2021-03-03 00:59:11 -0600

    Derive `Serialize` and `Deserialize` on the interaction types.

--- crates/core/src/interaction.rs
+++ crates/core/src/interaction.rs
@@ -1,11 +1,13 @@
-#[derive(Debug)]
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
 pub enum InteractionType {
     Interact,
     Attack,
     InteractAt,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Serialize, Deserialize)]
 pub enum InteractHand {
     Main,
     Offhand,
